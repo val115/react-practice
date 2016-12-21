4de731d64670454549234bb84738459f3906eb38
Sony <fedval173@gmail.com>
2016-12-21 23:05:45 +0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import  React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './app';
+
+import { HomePage, HomeRoutes } from './pages/home/index';
+
+export  default (
+    <Route component={ App } path={ App.path } >
+        <IndexRoute component={ HomePage } />
+
+        { HomeRoutes }
+    </Route>
+);
